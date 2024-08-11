61c8c8af766bb48a0e43c05f7e80ff5f7945fc9f
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-11 03:49:53 -0600

    Menghapus folder meja, pelanggan, dan reservasi dari root proyek

---
+++ home.php
@@ -0,0 +1,148 @@
+            <!-- Start Body Wrapper -->
+            <div class="body-wrapper">
+                <div class="container-fluid">
+
+                    <!-- Start Breadcrumb -->
+                    <div class="card card-body py-3">
+                        <div class="row align-items-center">
+                            <div class="col-12">
+                                <div class="d-sm-flex align-items-center justify-space-between">
+                                    <h4 class="mb-4 mb-sm-0 card-title">Home</h4>
+                                    <nav aria-label="breadcrumb" class="ms-auto">
+                                        <ol class="breadcrumb">
+                                            <li class="breadcrumb-item d-flex align-items-center">
+                                                <a class="text-muted text-decoration-none d-flex" href="./">
+                                                    <iconify-icon icon="solar:home-2-line-duotone" class="fs-6"></iconify-icon>
+                                                </a>
+                                            </li>
+                                            <!-- <iconify-icon icon="solar:alt-arrow-right-outline" class="mx-1 my-auto" width="20" height="20"></iconify-icon> -->
+                                             <iconify-icon icon="ic:sharp-keyboard-arrow-right" class="mx-1" width="26" height="26"></iconify-icon>
+                                            <li class="breadcrumb-item my-auto" aria-current="page">
+                                                <span class="badge fw-medium fs-2 bg-primary-subtle text-primary"> Data </span>
+                                            </li>
+                                        </ol>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Breadcrumb -->
+
+                    <div class="widget-content searchable-container list">
+
+                        <!-- Start Card Header -->
+                        <div class="card card-body">
+                            <div class="row">
+                                <div class="col-md-4 col-xl-3">
+                                    <form class="position-relative">
+                                        <input type="text" class="form-control product-search ps-5" id="input-search" placeholder="Search Data..." />
+                                        <!-- <i class="ti ti-search position-absolute top-50 start-0 translate-middle-y fs-6 text-dark ms-3"></i> -->
+                                        <iconify-icon icon="tabler:search" class="position-absolute top-50 start-0 translate-middle-y fs-6 text-dark ms-3"></iconify-icon>
+                                    </form>
+                                </div>
+                                <div class="col-md-8 col-xl-9 text-end d-flex justify-content-md-end justify-content-center mt-3 mt-md-0">
+                                    <a href="javascript:void(0)" id="btn-add-data" class="btn btn-primary d-flex align-items-center">
+                                        <!-- <i class="ti ti-users text-white me-1 fs-5"></i> Add Contact  -->
+                                        <iconify-icon icon="tabler:users" class="text-white me-1 fs-5"></iconify-icon>Add Data
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Card Header -->
+                        
+                        <!-- Start Card body main content -->
+                        <div class="card card-body">
+                            <div class="table-responsive">
+                                <table class="table search-table align-middle text-nowrap">
+                                    <thead class="header-item">
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Location</th>
+                                        <th>Phone</th>
+                                        <th>Action</th>
+                                    </thead>
+                                    <tbody>
+                                        <!-- start row -->
+                                        <tr class="search-items">
+                                            <td>1</td>
+                                            <td>
+                                                <div class="d-flex align-items-center">
+                                                    <img src="./assets/images/profile/user-10.jpg" alt="avatar" class="rounded-circle" width="35" />
+                                                    <div class="ms-3">
+                                                        <div class="user-meta-info">
+                                                            <h6 class="user-name mb-0" data-name="Emma Adams">Emma Adams</h6>
+                                                            <span class="user-work fs-3" data-occupation="Web Developer">Web Developer</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <span class="usr-email-addr" data-email="adams@mail.com">adams@mail.com</span>
+                                            </td>
+                                            <td>
+                                                <span class="usr-location" data-location="Boston, USA">Boston, USA</span>
+                                            </td>
+                                            <td>
+                                                <span class="usr-ph-no" data-phone="+1 (070) 123-4567">+91 (070) 123-4567</span>
+                                            </td>
+                                            <td>
+                                                <div class="action-btn">
+                                                    <a href="javascript:void(0)" class="text-primary edit">
+                                                        <!-- <i class="ti ti-eye fs-5"></i> -->
+                                                        <iconify-icon icon="tabler:edit" class="fs-5"></iconify-icon>
+                                                    </a>
+                                                    <a href="javascript:void(0)" class="text-dark delete ms-2">
+                                                        <!-- <i class="ti ti-trash fs-5"></i> -->
+                                                        <iconify-icon icon="tabler:trash" class="fs-5"></iconify-icon>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <!-- end row -->
+                                        <!-- start row -->
+                                        <tr class="search-items">
+                                            <td>2</td>
+                                            <td>
+                                                <div class="d-flex align-items-center">
+                                                    <img src="./assets/images/profile/user-2.jpg" alt="avatar" class="rounded-circle" width="35" />
+                                                    <div class="ms-3">
+                                                        <div class="user-meta-info">
+                                                            <h6 class="user-name mb-0" data-name="Olivia Allen">Olivia Allen</h6>
+                                                            <span class="user-work fs-3" data-occupation="Web Designer">Web Designer</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <span class="usr-email-addr" data-email="allen@mail.com">allen@mail.com</span>
+                                            </td>
+                                            <td>
+                                                <span class="usr-location" data-location="Sydney, Australia">Sydney, Australia</span>
+                                            </td>
+                                            <td>
+                                                <span class="usr-ph-no" data-phone="+91 (125) 450-1500">+91 (125) 450-1500</span>
+                                            </td>
+                                            <td>
+                                                <div class="action-btn">
+                                                    <a href="javascript:void(0)" class="text-primary edit">
+                                                        <iconify-icon icon="tabler:edit" class="fs-5"></iconify-icon>
+                                                    </a>
+                                                    <a href="javascript:void(0)" class="text-dark delete ms-2">
+                                                        <iconify-icon icon="tabler:trash" class="fs-5"></iconify-icon>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <!-- end row -->
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- ENd Card body main content -->
+                    </div>
+                </div>
+                
+            </div>
+            <!-- End Body Wrapper -->
+        
