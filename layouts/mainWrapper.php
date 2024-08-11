633b40f61339443077a78c1cdd6f37948ff80a6d
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-11 11:20:49 +0800

    Memindahkan struktur HTML utama ke mainWrapper.php dan memperbarui index.php

---
+++ layouts/mainWrapper.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light" data-layout="vertical">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>APP Admin Dashboard</title>
+    
+    <!-- Core Css -->
+    <link rel="stylesheet" href="./assets/css/styles.css">
+
+    <style>
+        html {
+            display: none; 
+        }
+    </style>
+    <script>
+        (function() {
+            const savedTheme = localStorage.getItem('selectedTheme') || 'light';
+            document.documentElement.setAttribute('data-bs-theme', savedTheme);
+            document.documentElement.style.display = 'block'; 
+        })();
+    </script>
+</head>
+<body class="link-sidebar">
+
+    <!-- Main Wrapper Start -->
+    <div id="main-wrapper">
