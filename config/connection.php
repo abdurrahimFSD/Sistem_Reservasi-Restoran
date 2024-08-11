3edb4dddc6a3339071027bb742d23efef655fb42
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-11 03:54:35 -0600

    Menambahkan kode konfigurasi koneksi database di file connection.php

---
+++ config/connection.php
@@ -0,0 +1,8 @@
+<?php
+$serverName = "localhost";
+$username = "root";
+$password = "";
+$databaseName = "sistem_reservasi-restoran";
+
+$connection = mysqli_connect($serverName, $username, $password, $databaseName);
+?>
