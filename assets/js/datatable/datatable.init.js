6bde53f974c527b413243c00a0f4b34bc90da9da
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-13 20:35:33 -0600

    Mengganti nama file menjadi datatable.init.js

---
+++ assets/js/datatable/datatable.init.js
@@ -0,0 +1,10 @@
+new DataTable("#dataTables", {
+    language: {
+        pageLength: "Show _MENU_ articles per page",
+    },
+    lengthMenu: [
+        [5, 10, 15, 20],
+        [5, 10, 15, 20],
+    ],
+    pageLength: 5, // 5 entri Default
+});
