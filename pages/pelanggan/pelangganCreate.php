d08a2bb7846305f617d8eb1507721f3388550d07
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-13 22:21:41 -0600

    Mengubah link kembali dari halaman 'mejaData' ke 'pelangganData' pada halaman tambah pelanggan

--- pages/pelanggan/pelangganCreate.php
+++ pages/pelanggan/pelangganCreate.php
@@ -59,7 +59,7 @@
                         <label for="noTelepon" class="form-label">No Telepon</label>
                         <input type="number" class="form-control" name="noTelepon" id="noTelepon" placeholder="081347200001" required>
                     </div>
-                    <a href="?page=mejaData" class="d-inline-flex justify-content-center align-items-center btn btn-outline-secondary me-2">
+                    <a href="?page=pelangganData" class="d-inline-flex justify-content-center align-items-center btn btn-outline-secondary me-2">
                         <iconify-icon icon="fluent:arrow-left-24-filled" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Kembali
                     </a>
                     <button type="submit" name="simpan" value="mejaCreate" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
