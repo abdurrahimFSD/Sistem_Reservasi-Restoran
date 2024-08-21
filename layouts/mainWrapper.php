5c76b97d2cf54e2e1d0204fdc0b25b2efb6be268
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-21 03:08:00 -0600

    Menambahkan import sweetalert

--- layouts/mainWrapper.php
+++ layouts/mainWrapper.php
@@ -10,6 +10,9 @@
     <!-- Core Css -->
     <link rel="stylesheet" href="./assets/css/style.css">
 
+    <!-- Sweetalert -->
+    <link rel="stylesheet" href="./assets/libs/sweetalert/sweetalert2.min.css">
+
     <!-- Datatable -->
     <link rel="stylesheet" href="./assets/libs/DataTables/datatables.css">
 
