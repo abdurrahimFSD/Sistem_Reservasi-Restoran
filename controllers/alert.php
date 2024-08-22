f846862e003f7a22b8172dd8dc0ab8bfc3f3f86f
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-22 06:45:24 -0600

    Menambahkan alert untuk nomor meja yang duplikat saat penambahan data meja.

--- controllers/alert.php
+++ controllers/alert.php
@@ -15,6 +15,20 @@ if (isset($_GET['status'])) {
             });
         </script>
 <?php
+    } elseif ($_GET['status'] == 'duplicateMejaCreate' && isset($_GET['no_meja'])) {
+?>
+        <script>
+            Swal.fire({
+                title: 'Gagal!',
+                text: 'No Meja <?php echo $_GET['no_meja']; ?> sudah ada, tidak boleh sama!',
+                icon: 'warning'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = './index.php?page=mejaCreate';
+                }
+            });
+        </script>
+<?php
     } elseif ($_GET['status'] == 'errorMejaCreate') {
 ?>
         <script>
