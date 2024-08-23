a88575768854ff4713cbc6ddd357bcfb39bf71dd
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-23 21:42:20 +0800

    Mengganti button submit dengan button biasa dan menambahkan input hidden untuk nilai 'simpan' di pelangganUpdate.php

--- pages/pelanggan/pelangganUpdate.php
+++ pages/pelanggan/pelangganUpdate.php
@@ -75,7 +75,8 @@
                     <a href="?page=pelangganData" class="d-inline-flex justify-content-center align-items-center btn btn-outline-secondary me-2">
                         <iconify-icon icon="fluent:arrow-left-24-filled" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Kembali
                     </a>
-                    <button type="submit" name="simpan" value="pelangganUpdate" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
+                    <input type="hidden" name="simpan" value="pelangganUpdate">
+                    <button type="button" id="simpanButton" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
                         <iconify-icon icon="fluent:save-24-regular" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Simpan
                     </button>
                 </form>
