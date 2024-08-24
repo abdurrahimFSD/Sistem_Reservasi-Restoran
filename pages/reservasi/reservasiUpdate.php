65ab6dffa2cf011b0d6412432d416d77c0d9a753
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-24 06:46:14 -0600

    Mengganti button type submit menjadi type button dan menambahkan input hidden untuk reservasiUpdate

--- pages/reservasi/reservasiUpdate.php
+++ pages/reservasi/reservasiUpdate.php
@@ -111,7 +111,8 @@ if (isset($_GET['id_reservasi'])) {
                     <a href="?page=reservasiData" class="d-inline-flex justify-content-center align-items-center btn btn-outline-secondary me-2">
                         <iconify-icon icon="fluent:arrow-left-24-filled" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Kembali
                     </a>
-                    <button type="submit" name="simpan" value="reservasiUpdate" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
+                    <input type="hidden" name="simpan" value="reservasiUpdate">
+                    <button type="button" id="simpanButton" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
                         <iconify-icon icon="fluent:save-24-regular" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Simpan
                     </button>
                 </form>
