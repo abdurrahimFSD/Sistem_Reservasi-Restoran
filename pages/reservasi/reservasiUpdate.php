b9aff12920cb11605294c22819fb3999ea7b4c5c
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-24 07:17:58 -0600

    Menambahkan konfirmasi Sweetalert2 dan AJAX untuk proses update reservasi pada reservasiUpdate.php

--- pages/reservasi/reservasiUpdate.php
+++ pages/reservasi/reservasiUpdate.php
@@ -123,3 +123,41 @@ if (isset($_GET['id_reservasi'])) {
     
 </div>
 <!-- End Body Wrapper -->
+
+<!-- Alert reservasiUpdate -->
+<script>
+    document.getElementById('simpanButton').addEventListener('click', function() {
+        Swal.fire({
+            title: 'Apakah anda ingin menyimpan perubahan ini?',
+            showCancelButton: true,
+            confirmButtonText: 'Simpan',
+            cancelButtonText: 'Batal',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Jika user mengkonfirmasi 'Simpan', kirim form melalui AJAX
+                const form = document.getElementById('reservasiUpdateForm');
+                const formData = new FormData(form);
+
+                fetch('./controllers/process.php', {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => response.text())
+                .then(response => {
+                    if (response === 'successReservasiUpdate') {
+                        Swal.fire('Tersimpan!', '', 'success').then(() => {
+                            window.location.href = './index.php?page=reservasiData';
+                        });
+                    } else if (response === 'errorReservasiUpdate') {
+                        Swal.fire('Gagal', 'Data reservasi gagal diedit', 'error');
+                    }
+                })
+                .catch(error => {
+                    Swal.fire('Gagal', 'Terjadi Kesalahan', 'error');
+                });
+            } else if (result.isDismissed) {
+                Swal.fire('Perubahan dibatalkan', '', 'info');
+            }
+        });
+    });
+</script>
