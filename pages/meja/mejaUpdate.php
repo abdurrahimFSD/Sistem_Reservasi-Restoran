4c49058445236584a17690d8d39aa6974004e59d
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-25 03:47:49 -0600

    Memperbarui id menjadi simpanMejaUpdate

--- pages/meja/mejaUpdate.php
+++ pages/meja/mejaUpdate.php
@@ -83,7 +83,7 @@
                         <iconify-icon icon="fluent:arrow-left-24-filled" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Kembali
                     </a>
                     <input type="hidden" name="simpan" value="mejaUpdate">
-                    <button type="button" id="simpanButton" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
+                    <button type="button" id="simpanMejaUpdate" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
                         <iconify-icon icon="fluent:save-24-regular" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Simpan
                     </button>
                 </form>
@@ -94,41 +94,3 @@
     
 </div>
 <!-- End Body Wrapper -->
-
-<!-- Alert -->
-<script>
-document.getElementById('simpanButton').addEventListener('click', function() {
-    Swal.fire({
-        title: 'Apakah anda ingin menyimpan perubahan ini?',
-        showCancelButton: true, // Menampilkan tombol "Cancel"
-        confirmButtonText: 'Simpan', // Tombol "Simpan"
-        cancelButtonText: 'Batal', // Tombol "Batal"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // Jika pengguna mengkonfirmasi "Simpan", kirim formulir melalui AJAX
-            const form = document.getElementById('mejaUpdateForm');
-            const formData = new FormData(form);
-
-            fetch('./controllers/process.php', {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => response.text())
-            .then(response => {
-                if (response === 'successMejaUpdate') {
-                    Swal.fire('Tersimpan!', '', 'success').then(() => {
-                        window.location.href = './index.php?page=mejaData';
-                    });
-                } else if (response === 'errorMejaUpdate') {
-                    Swal.fire('Gagal!', 'Data meja gagal diedit', 'error');
-                }
-            })
-            .catch(error => {
-                Swal.fire('Gagal!', 'Terjadi kesalahan', 'error');
-            });
-        } else if (result.isDismissed) {
-            Swal.fire('Perubahan dibatalkan', '', 'info');
-        }
-    });
-});
-</script>
