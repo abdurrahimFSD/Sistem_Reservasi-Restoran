d7e659efe44695eae218257e8ed4494540e4f401
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-26 10:46:29 +0800

    Refactor alert.js: Menyederhanakan alert update dengan function setupUpdateButton yang dapat digunakan kembali

--- components/alert/alert.js
+++ components/alert/alert.js
@@ -56,113 +56,57 @@ if (status === 'successMejaCreate') {
     });
 }
 
+
 // Kode alert untuk update
-if (document.getElementById('simpanMejaUpdate')) {
-    document.getElementById('simpanMejaUpdate').addEventListener('click', function() {
-        Swal.fire({
-            title: 'Apakah anda ingin menyimpan perubahan ini?',
-            showCancelButton: true, // Menampilkan tombol "Cancel"
-            confirmButtonText: 'Simpan', // Tombol "Simpan"
-            cancelButtonText: 'Batal', // Tombol "Batal"
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Jika pengguna mengkonfirmasi "Simpan", kirim formulir melalui AJAX
-                const form = document.getElementById('mejaUpdateForm');
-                const formData = new FormData(form);
-    
-                fetch('./controllers/process.php', {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.text())
-                .then(response => {
-                    if (response === 'successMejaUpdate') {
-                        Swal.fire('Tersimpan!', '', 'success').then(() => {
-                            window.location.href = './index.php?page=mejaData';
-                        });
-                    } else if (response === 'errorMejaUpdate') {
-                        Swal.fire('Gagal!', 'Data meja gagal diedit', 'error');
-                    }
-                })
-                .catch(error => {
-                    Swal.fire('Gagal!', 'Terjadi kesalahan', 'error');
-                });
-            } else if (result.isDismissed) {
-                Swal.fire('Perubahan dibatalkan', '', 'info');
-            }
+function setupUpdateButton(buttonId, formId, successStatus, successRedirectPage, successMessage, errorStatus) {
+    const button = document.getElementById(buttonId);
+    if (button) {
+        button.addEventListener('click', function() {
+            Swal.fire({
+                title: 'Apakah Anda ingin menyimpan perubahan ini?',
+                showCancelButton: true,
+                confirmButtonText: 'Simpan',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Jika pengguna mengkonfirmasi "Simpan", kirim formulir melalui AJAX
+                    const form = document.getElementById(formId);
+                    const formData = new FormData(form);
+
+                    fetch('./controllers/process.php', {
+                        method: 'POST',
+                        body: formData
+                    })
+                    .then(response => response.text())
+                    .then(response => {
+                        if (response === successStatus) {
+                            Swal.fire('Tersimpan', successMessage, 'success').then(() => {
+                                window.location.href = `./index.php?page=${successRedirectPage}`;
+                            });
+                        } else if (response === errorStatus) {
+                            Swal.fire('Gagal!', '', 'error');
+                        }
+                    })
+                    .catch(error => {
+                        Swal.fire('Gagal!', 'Terjadi kesalahan.', 'error');
+                    });
+                } else if (result.isDismissed) {
+                    Swal.fire('Perubahan dibatalkan', '', 'info');
+                }
+            });
         });
-    });
-} else if (document.getElementById('simpanPelangganUpdate')) {
-    document.getElementById('simpanPelangganUpdate').addEventListener('click', function() {
-        Swal.fire({
-            title: 'Apakah anda ingin menyimpan perubahan ini?',
-            showCancelButton: true,
-            confirmButtonText: 'Simpan',
-            cancelButtonText: 'Batal'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Jika user mengkonfirmasi 'Simpan', kirim form melalui AJAX
-                const form = document.getElementById('pelangganUpdateForm');
-                const formData = new FormData(form);
-    
-                fetch('./controllers/process.php', {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.text())
-                .then(response => {
-                    if (response === 'successPelangganUpdate') {
-                        Swal.fire('Tersimpan!', '', 'success').then(() => {
-                            window.location.href = './index.php?page=pelangganData';
-                        });
-                    } else if (response === 'errorPelangganUpdate') {
-                        Swal.fire('Gagal', 'Data pelanggan gagal diedit', 'error');
-                    }
-                })
-                .catch(error => {
-                    Swal.fire('Gagal', 'Terjadi Kesalahan', 'error');
-                });
-            } else if (result.isDismissed) {
-                Swal.fire('Perubahan dibatalkan', '', 'info');
-            }
-        });
-    });
-} else if (document.getElementById('simpanReservasiUpdate')) {
-    document.getElementById('simpanReservasiUpdate').addEventListener('click', function() {
-        Swal.fire({
-            title: 'Apakah anda ingin menyimpan perubahan ini?',
-            showCancelButton: true,
-            confirmButtonText: 'Simpan',
-            cancelButtonText: 'Batal',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Jika user mengkonfirmasi 'Simpan', kirim form melalui AJAX
-                const form = document.getElementById('reservasiUpdateForm');
-                const formData = new FormData(form);
-    
-                fetch('./controllers/process.php', {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.text())
-                .then(response => {
-                    if (response === 'successReservasiUpdate') {
-                        Swal.fire('Tersimpan!', '', 'success').then(() => {
-                            window.location.href = './index.php?page=reservasiData';
-                        });
-                    } else if (response === 'errorReservasiUpdate') {
-                        Swal.fire('Gagal', 'Data reservasi gagal diedit', 'error');
-                    }
-                })
-                .catch(error => {
-                    Swal.fire('Gagal', 'Terjadi Kesalahan', 'error');
-                });
-            } else if (result.isDismissed) {
-                Swal.fire('Perubahan dibatalkan', '', 'info');
-            }
-        });
-    });
+    }
 }
+
+// Mengatur tombol update untuk meja
+setupUpdateButton('simpanMejaUpdate', 'mejaUpdateForm', 'successMejaUpdate' ,'mejaData', 'Data meja berhasil diedit', 'errorMejaUpdate');
+
+// Mengatur tombol update untuk pelanggan
+setupUpdateButton('simpanPelangganUpdate', 'pelangganUpdateForm', 'successPelangganUpdate' ,'pelangganData', 'Data pelanggan berhasil diedit', 'errorPelangganUpdate');
+
+// Mengatur tombol update untuk reservasi
+setupUpdateButton('simpanReservasiUpdate', 'reservasiUpdateForm', 'successReservasiUpdate' ,'reservasiData', 'Data reservasi berhasil diedit', 'errorReservasiUpdate');
+
 
 // Kode alert untuk delete
 if (document.getElementById('deleteButtonMeja') || 
