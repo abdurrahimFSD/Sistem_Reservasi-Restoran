f9774bda998be3a756639e4d83e4f1fd1543cdc9
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-28 21:16:41 +0800

    Memperbarui script datatable.init.js

--- assets/js/datatable/datatable.init.js
+++ assets/js/datatable/datatable.init.js
@@ -1,10 +1,6 @@
-new DataTable("#dataTables", {
-    language: {
-        pageLength: "Show _MENU_ articles per page",
-    },
-    lengthMenu: [
-        [5, 10, 15, 20],
-        [5, 10, 15, 20],
-    ],
-    pageLength: 5, // 5 entri Default
+$(document).ready(function() {
+    $('#dataTables').DataTable({
+        "pageLength": 5, 
+        "lengthMenu": [5, 10, 15, 20, 25, 50, 100]
+    });
 });
