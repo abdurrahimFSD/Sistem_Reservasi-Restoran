099bc9b243e3f4c8b9fe39df9a62f2f557d6b778
Abdurrahim <abdurrahim22code@gmail.com>
2024-08-30 20:40:23 +0800

    Menambahkan function signup untuk menangani proses signup

--- controllers/authController.php
+++ controllers/authController.php
@@ -1,4 +1,18 @@
 <?php
 include('../../config/connection.php');
 
+// Function untuk menangani signup
+function signup($username, $email, $password) {
+    global $connection;
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+    $stmt = $connection->prepare($query);
+    $stmt->bind_param("sss", $username, $email, $hashedPassword);
+    if($stmt->execute()) {
+        return true;
+    } else {
+        return false;
+    }
+}
 ?>
