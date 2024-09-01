7f883df5557c274aefe3779013c1a2b3a040f04e
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-01 21:30:42 +0800

    Menambahkan element div untuk menampilkan pesan error jika email sudah digunakan

--- pages/auth/signup.php
+++ pages/auth/signup.php
@@ -67,6 +67,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                     <div class="mb-3">
                                         <label for="email" class="form-label">Email Address</label>
                                         <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" required>
+                                        <div id="emailError" class="text-danger mt-1 fw-bold"></div>
                                     </div>
                                     <div class="mb-4">
                                         <label for="password" class="form-label">Password</label>
