0aeb8f9694ddcbf76748ccc6af232184fadc91c3
Abdurrahim <abdurrahim22code@gmail.com>
2024-09-01 10:54:41 +0800

    Menghapus session_start di function signin

--- controllers/authController.php
+++ controllers/authController.php
@@ -28,7 +28,6 @@ function signin($username, $password) {
     if($result->num_rows > 0) {
         $user = $result->fetch_assoc();
         if(password_verify($password, $user['password'])) {
-            session_start();
             // Menyimpan user_id, username, dan email ke dalam session
             $_SESSION['user_id'] = $user['id_user'];
             $_SESSION['username'] = $user['username'];
