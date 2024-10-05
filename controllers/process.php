8e8244ad2068f3b0628d5ae36aa2fb8ac3cde38d
Abdurrahim <abdurrahim22code@gmail.com>
2024-10-05 17:20:55 +0700

    Mengubah response pelangganCreate di process.php dari header location menjadi output string status

--- controllers/process.php
+++ controllers/process.php
@@ -37,9 +37,9 @@ if (isset($_POST['simpan'])) {
 
         // Jika proses create berhasil
         if ($result == 'success') {
-            header("Location: ../index.php?page=pelangganCreate&status=successPelangganCreate");
-        } else {
-            header("Location: ../index.php?page=PelangganCreate&status=errorPelangganCreate");
+            echo 'successPelangganCreate';
+        } elseif ($result == 'error') {
+            echo 'errorPelangganCreate';
         }
     } elseif ($_POST['simpan'] == 'pelangganUpdate') {  // Jika tombol simpan adalah pelangganUpdate
         // Memanggil fungsi pelangganUpdate
