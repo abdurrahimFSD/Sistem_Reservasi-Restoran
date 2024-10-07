808a83d798042c53f06f35a24d848a1f5d4d4ff8
Abdurrahim <abdurrahim22code@gmail.com>
2024-10-07 09:14:05 +0700

    Mengubah tombol simpan dari type 'button' menjadi 'submit' untuk form dengan id 'reservasiUpdateForm'

--- pages/reservasi/reservasiUpdate.php
+++ pages/reservasi/reservasiUpdate.php
@@ -112,7 +112,7 @@ if (isset($_GET['id_reservasi'])) {
                         <iconify-icon icon="fluent:arrow-left-24-filled" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Kembali
                     </a>
                     <input type="hidden" name="simpan" value="reservasiUpdate">
-                    <button type="button" id="simpanReservasiUpdate" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
+                    <button type="submit" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
                         <iconify-icon icon="fluent:save-24-regular" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Simpan
                     </button>
                 </form>
